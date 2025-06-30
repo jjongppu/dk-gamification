08bf0a32424b1084f9006bbf88b5d6e90b457811
jjongppu <jjongppu@naver.com>
2025-06-30 15:19:03 +0900

    Update migration to handle existing records

--- db/migrate/20250103120000_add_reason_to_gamification_score_events.rb
+++ db/migrate/20250103120000_add_reason_to_gamification_score_events.rb
@@ -1,5 +1,17 @@
+# frozen_string_literal: true
+
 class AddReasonToGamificationScoreEvents < ActiveRecord::Migration[7.0]
-  def change
-    add_column :gamification_score_events, :reason, :string, null: false, default: ""
+  def up
+    add_column :gamification_score_events, :reason, :string, default: "", null: true
+    execute <<~SQL
+      UPDATE gamification_score_events
+      SET reason = ''
+      WHERE reason IS NULL
+    SQL
+    change_column_null :gamification_score_events, :reason, false
+  end
+
+  def down
+    remove_column :gamification_score_events, :reason
   end
 end
