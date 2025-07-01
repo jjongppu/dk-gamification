535926d71f5bd5c87a0c341e111b5edc08b3f9d7
jonggil.kim <jonggil.kim@dkbmc.com>
2025-07-01 15:24:04 +0900

    레벨 프로그래스바 / 들여쓰기

--- app/models/discourse_gamification/gamification_level_info.rb
+++ app/models/discourse_gamification/gamification_level_info.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 module DiscourseGamification
-    class GamificationLevelInfo < ActiveRecord::Base
-      self.table_name = "gamification_level_infos"
-    end
+  class GamificationLevelInfo < ActiveRecord::Base
+    self.table_name = "gamification_level_infos"
   end
-  
+end
