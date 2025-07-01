6752a28c675d6398d76a0292f77a3708ac9ad987
jonggil.kim <jonggil.kim@dkbmc.com>
2025-07-01 15:17:57 +0900

    레벨 프로그래스바 추가 +log추가

---
+++ app/models/discourse_gamification/gamification_level_info.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module DiscourseGamification
+    class GamificationLevelInfo < ActiveRecord::Base
+      self.table_name = "gamification_level_infos"
+    end
+  end
+  
