63636dbdf3f20592cda02d742d6f6c72cdb5e3b5
jonggil.kim <jonggil.kim@dkbmc.com>
2025-07-03 10:44:39 +0900

    nil 예외처리

--- assets/javascripts/initializers/add-user-level-icon.js
+++ assets/javascripts/initializers/add-user-level-icon.js
@@ -1,25 +1,15 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
 
 export default {
-  name: "add-user-level-icon",
+  name: "add-user-level-text",
 
   initialize() {
     withPluginApi("0.8.25", (api) => {
-      api.decorateWidget("user-name:before", (dec) => {
-        const user = dec.attrs.user;
-        console.log(' !!!! user', user);
-        const imageUrl = user?.user_level_info?.image_url;
-
-        if (imageUrl) {
-          return dec.h("img.user-level-icon", {
-            attributes: {
-              src: imageUrl,
-              style:
-                "width: 20px; height: 20px; margin-right: 4px; border-radius: 50%; vertical-align: middle;",
-            },
-          });
-        }
+      api.decorateWidget("poster-name:before", (helper) => {
+        return helper.h("span", {
+          style: "color: red; margin-left: 5px;"
+        }, "A");
       });
     });
-  },
+  }
 };
