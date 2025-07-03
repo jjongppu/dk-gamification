0e5e0bd7c41cce19e8188bfef1e3b06eff6c15bb
jonggil.kim <jonggil.kim@dkbmc.com>
2025-07-03 09:18:52 +0900

    헬퍼코드수정

--- lib/discourse_gamification/level_helper.rb
+++ lib/discourse_gamification/level_helper.rb
@@ -1,32 +1,38 @@
 # frozen_string_literal: true
 
 module DiscourseGamification
-    class LevelHelper
+  class LevelHelper
 
-      # plugins/discourse-gamification/lib/discourse_gamification/level_helper.rb
-      def self.progress_for(user_id)
-        score = GamificationScore.find_by(user_id: user_id)&.score || 0
-        level_info = GamificationLevelInfo.where(":score BETWEEN min_score AND max_score", score: score).first
-  
-        return {
-          level: nil, progress_percent: 0, current_score: score,
-          min: 0, max: 0, name: nil, image_url: nil
-        } unless level_info
-  
-        min = level_info.min_score
-        max = level_info.max_score
-        progress = ((score - min).to_f / (max - min) * 100).round
-  
-        {
-          level: level_info.level,
-          progress_percent: progress,
-          current_score: score,
-          min: min,
-          max: max,
-          name: level_info.name,
-          image_url: level_info.image_url
-        }
-      end
+    def self.progress_for(user_id)
+      score = GamificationScore.find_by(user_id: user_id)&.score || 0
+
+      level_info = GamificationLevelInfo
+        .where("min_score <= :score AND max_score >= :score", score: score)
+        .first
+
+      return {
+        level: nil,
+        progress_percent: 0,
+        current_score: score,
+        min: 0,
+        max: 0,
+        name: nil,
+        image_url: nil
+      } unless level_info
+
+      min = level_info.min_score
+      max = level_info.max_score
+      progress = ((score - min).to_f / (max - min) * 100).round
+
+      {
+        level: level_info.level,
+        progress_percent: progress,
+        current_score: score,
+        min: min,
+        max: max,
+        name: level_info.name,
+        image_url: level_info.image_url
+      }
     end
   end
-  
+end
