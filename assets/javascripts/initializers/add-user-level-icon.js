419e67ee58838801769ced3152fd1d1a8dec8275
jonggil.kim home <jonggil.kim@dkbmc.com>
2025-07-05 23:58:09 +0900

    수정

--- assets/javascripts/initializers/add-user-level-icon.js
+++ assets/javascripts/initializers/add-user-level-icon.js
@@ -7,7 +7,7 @@ export default {
     withPluginApi("0.8.25", (api) => {
       api.decorateWidget("poster-name:before", (helper) => {
         // const user = helper.attrs.user;
-        const levelInfo = helper.attrs.gamification_level_info; // post에 직접 포함된 속성으로 변경됨!
+        const levelInfo = helper.getModel().gamification_level_info; // post에 직접 포함된 속성으로 변경됨!
 
         console.log('1.helper.attrs ::::', helper.attrs);
         console.log('2.gamification_level_info::::', levelInfo);
