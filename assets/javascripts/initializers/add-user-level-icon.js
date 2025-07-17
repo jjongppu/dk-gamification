61cc92f1365d06d1c27d5a84540d3c1a2c927d38
jjongppu <jjongppu@naver.com>
2025-07-17 19:20:45 +0900

    Remove unrelated features and keep level display

--- assets/javascripts/initializers/add-user-level-icon.js
+++ assets/javascripts/initializers/add-user-level-icon.js
@@ -8,20 +8,15 @@ export default {
       api.decorateWidget("poster-name:before", (helper) => {
         const post = helper.getModel?.() || helper.attrs.post || helper.attrs;
         const levelInfo = post.gamification_level_info;
-        console.log('2.gamification_level_info::::', levelInfo);
         if (!levelInfo) return;
 
         const image = helper.h("img", {
           src: levelInfo.image_url,
-          style: "width: 24px; height: 24px; margin-right: 2px;"
+          style: "width: 24px; height: 24px; margin-right: 2px;",
         });
 
-        // const text = helper.h("span", {
-        //   style: "color: #f8c100;"
-        // }, `${levelInfo.name} LV.${levelInfo.level} ${Math.floor(levelInfo.progress_percent)}%`);
-
-        return helper.h("span", { style: " margin-right: 2px !important;" }, [image]);
+        return helper.h("span", { style: "margin-right: 2px" }, [image]);
       });
     });
-  }
+  },
 };
--- jobs/regular/update_stale_leaderboard_positions.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Jobs
-  class UpdateStaleLeaderboardPositions < ::Jobs::Base
-    def execute(args = nil)
-      DKGamification::LeaderboardCachedView.update_all
-    end
-  end
-end
