271ffcf7cf5db3e589ae9301c40eb22e36223321
jonggil.kim <jonggil.kim@dkbmc.com>
2025-07-17 18:54:54 +0900

    초기 적재

--- app/controllers/dk_gamification/admin_gamification_score_event_controller.rb
+++ app/controllers/dk_gamification/admin_gamification_score_event_controller.rb
@@ -1,53 +1,54 @@
 # frozen_string_literal: true
+module ::DKGamification
+  class DKGamification::AdminGamificationScoreEventController < Admin::AdminController
+    requires_plugin DKGamification::PLUGIN_NAME
 
-class DKGamification::AdminGamificationScoreEventController < Admin::AdminController
-  requires_plugin DKGamification::PLUGIN_NAME
+    def show
+      params.permit(%i[id user_id date])
 
-  def show
-    params.permit(%i[id user_id date])
+      events = DKGamification::GamificationScoreEvent.limit(100)
+      events = events.where(id: params[:id]) if params[:id]
+      events = events.where(user_id: params[:user_id]) if params[:user_id]
+      events = events.where(date: params[:date]) if params[:date]
 
-    events = DKGamification::GamificationScoreEvent.limit(100)
-    events = events.where(id: params[:id]) if params[:id]
-    events = events.where(user_id: params[:user_id]) if params[:user_id]
-    events = events.where(date: params[:date]) if params[:date]
+      raise Discourse::NotFound unless events
 
-    raise Discourse::NotFound unless events
+      render_serialized({ events: events }, AdminGamificationScoreEventIndexSerializer, root: false)
+    end
 
-    render_serialized({ events: events }, AdminGamificationScoreEventIndexSerializer, root: false)
-  end
+    def create
+      params.require(%i[user_id date points reason])
+      params.permit(:description, :related_id, :related_type)
 
-  def create
-    params.require(%i[user_id date points reason])
-    params.permit(:description, :related_id, :related_type)
+      event = DKGamification::GamificationScoreEvent.record!(
+        user_id: params[:user_id],
+        date: params[:date],
+        points: params[:points],
+        reason: params[:reason],
+        description: params[:description],
+        related_id: params[:related_id],
+        related_type: params[:related_type],
+      )
 
-    event = DKGamification::GamificationScoreEvent.record!(
-      user_id: params[:user_id],
-      date: params[:date],
-      points: params[:points],
-      reason: params[:reason],
-      description: params[:description],
-      related_id: params[:related_id],
-      related_type: params[:related_type],
-    )
+      render_serialized(event, AdminGamificationScoreEventSerializer, root: false)
+    end
 
-    render_serialized(event, AdminGamificationScoreEventSerializer, root: false)
-  end
+    def update
+      params.require(%i[id points reason])
+      params.permit(:description, :related_id, :related_type)
 
-  def update
-    params.require(%i[id points reason])
-    params.permit(:description, :related_id, :related_type)
+      event = DKGamification::GamificationScoreEvent.find(params[:id])
+      raise Discourse::NotFound unless event
 
-    event = DKGamification::GamificationScoreEvent.find(params[:id])
-    raise Discourse::NotFound unless event
-
-    if event.update(points: params[:points],
-                    reason: params[:reason],
-                    description: params[:description] || event.description,
-                    related_id: params[:related_id],
-                    related_type: params[:related_type])
-      render json: success_json
-    else
-      render_json_error(event)
+      if event.update(points: params[:points],
+                      reason: params[:reason],
+                      description: params[:description] || event.description,
+                      related_id: params[:related_id],
+                      related_type: params[:related_type])
+        render json: success_json
+      else
+        render_json_error(event)
+      end
     end
   end
 end
