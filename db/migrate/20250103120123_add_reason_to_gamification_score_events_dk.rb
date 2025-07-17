c0fb51ca6d396e82031db7275ab4e1ee151b6948
jonggil.kim home <jonggil.kim@dkbmc.com>
2025-07-17 22:02:20 +0900

    소스수정

--- db/migrate/20250103120123_add_reason_to_gamification_score_events_dk.rb
+++ db/migrate/20250103120123_add_reason_to_gamification_score_events_dk.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-class AddReasonToGamificationScoreEvents < ActiveRecord::Migration[7.0]
+class AddReasonToGamificationScoreEventsDk < ActiveRecord::Migration[7.0]
   def up
     add_column :gamification_score_events, :reason, :string, default: "", null: true
     execute <<~SQL
--- db/migrate/20250701000111_add_related_fields_to_gamification_score_events_dk.rb
+++ db/migrate/20250701000111_add_related_fields_to_gamification_score_events_dk.rb
@@ -1,4 +1,4 @@
-class AddRelatedFieldsToGamificationScoreEvents < ActiveRecord::Migration[7.0]
+class AddRelatedFieldsToGamificationScoreEventsDk < ActiveRecord::Migration[7.0]
   def change
     add_column :gamification_score_events, :related_id, :text
     add_column :gamification_score_events, :related_type, :text
